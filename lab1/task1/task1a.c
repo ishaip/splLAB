5d05ef94bc03fb0e09469832b5d05d2f291e2aaf
picus@wincs.cs.bgu.ac.il <picus>
2022-03-27 19:34:53 +0300

    -stoped working for a sec

--- lab1/task1/task1a.c
+++ lab1/task1/task1a.c
@@ -3,14 +3,14 @@
 #include <stdbool.h>
 
 int main(int argc, char **argv) {
-    FILE *outputFile = stdout;
+    //FILE *outputFile = stdout;
     FILE *inputFile = stdin;
     char c =0;
     bool  runningDebug = false;
     bool encoderFlag = false;
     int plusMinusCase = 1;
     char encoder = 0;
-    char incoderChar =0;
+    //char incoderChar =0;
     char* output = "";
     int lettersChaged = 0;
 
@@ -19,15 +19,15 @@ int main(int argc, char **argv) {
     for (int i =1; i < argc;i++){
         if(strncmp(argv[i], "-D",2) ==0)
             runningDebug = true;
-
+        
         else if(strncmp(argv[i], "-e",2) ==0){
-            encoder =  argv[i] + 2;
+            encoder = (int) argv[i] + 2;
             encoderFlag = true;
             plusMinusCase = -1;
         }
-
+        
         else if(strncmp(argv[i], "+e",2) ==0){
-            encoder =  argv[i] + 2;
+            encoder = (int) argv[i] + 2;
             encoderFlag = true;
         }
         // else if (strncmp(argv[i], "-o", 2) == 0){
@@ -38,40 +38,40 @@ int main(int argc, char **argv) {
             inputFile = fopen(argv[i] + 2, "r");
         }
     }
-    if (runningDebug == true){
-        for(int i = 1; i>argc; i++){
-            fprintf(stderr, "%s", argv[i]);
+    // if (runningDebug == true){
+    //     for(int i = 1; i>argc; i++){
+    //         fprintf(stderr, "%s", argv[i]);
+    //     }
+    //     fprintf (stderr, " ");
+    // }
+    fprintf(stderr, " me");
+    if (encoderFlag){
+        while((c = fgetc(inputFile)) != EOF){        //not adding first chares encoder amount of times
+            if (encoderFlag && plusMinusCase == -1 && encoder >0)
+                    encoder = encoder -1;
+            else
+                strcat(output, &c);
         }
-        fprintf (stderr, " ");
-    }
 
-
-    while((c = fgetc(inputFile)) != EOF && encoderFlag){        //not adding first chares encoder amount of times
-        if (encoderFlag && plusMinusCase == -1 && encoder >0)
-                encoder = encoder -1;
-        else
-            strcat(output, c);
+        while (plusMinusCase ==1 && encoder >0)      //adding the fisrt char encoder amount of times
+        {
+            strcat(output, &output[0]);
+            encoder--;
+        }
     }
-
-    while (encoderFlag && plusMinusCase ==1 && encoder >0)      //adding the fisrt char encoder amount of times
-    {
-        strcat(output, output[0]);
-        encoder--;
-    }
-    
 
     while((c = fgetc(inputFile)) != EOF && !encoderFlag){
+
         if(runningDebug)
-             printf("%d",c);
+             fprintf(stderr, " %d" ,c );
         if( 'A' <= c && c <= 'Z'){
             c = 46; 
             lettersChaged ++;
         }
         if(runningDebug)
-            printf(" %d\n" ,c );
-        strcat(output , c);
+            fprintf(stderr, " %d\n" ,c );
+        printf("%c", c);
      }
     if (runningDebug)
-        printf("%d", lettersChaged);
-    printf("%s", output);
+        fprintf(stderr, "%d", lettersChaged);
 }
