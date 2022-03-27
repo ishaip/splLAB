a25a8bb7e94d9010f804b3c83aeb362f63562085
picus@wincs.cs.bgu.ac.il <picus>
2022-03-27 18:19:21 +0300

    -done-

--- lab1/task1/task1a.c
+++ lab1/task1/task1a.c
@@ -9,7 +9,10 @@ int main(int argc, char **argv) {
     bool  runningDebug = false;
     bool encoderFlag = false;
     int plusMinusCase = 1;
-    int encoder = 0;
+    char encoder = 0;
+    char incoderChar =0;
+    char* output = "";
+    int lettersChaged = 0;
 
 
 
@@ -27,25 +30,48 @@ int main(int argc, char **argv) {
             encoder =  argv[i] + 2;
             encoderFlag = true;
         }
-        else if (strncmp(argv[i], "-o", 2) == 0){
-            outputFile = fopen(argv[i] + 2, "w");
-        }
+        // else if (strncmp(argv[i], "-o", 2) == 0){
+        //     outputFile = fopen(argv[i] + 2, "w");
+        // }
 
         else if (strncmp(argv[i], "-i", 2) == 0){
             inputFile = fopen(argv[i] + 2, "r");
         }
     }
-    // if (runningDebug == true){
-    //     for(int i = 1; i>arg)
-    // }
-
-
-    while((c = fgetc(inputFile)) != EOF){
-        if( 'A' <= c && c >= 'Z')
-            c = 46; 
-        fputs(outputFile , c);
+    if (runningDebug == true){
+        for(int i = 1; i>argc; i++){
+            fprintf(stderr, "%s", argv[i]);
+        }
+        fprintf (stderr, " ");
     }
-    fclose(outputFile);
-    fclose(inputFile);
 
+
+    while((c = fgetc(inputFile)) != EOF && encoderFlag){        //not adding first chares encoder amount of times
+        if (encoderFlag && plusMinusCase == -1 && encoder >0)
+                encoder = encoder -1;
+        else
+            strcat(output, c);
+    }
+
+    while (encoderFlag && plusMinusCase ==1 && encoder >0)      //adding the fisrt char encoder amount of times
+    {
+        strcat(output, output[0]);
+        encoder--;
+    }
+    
+
+    while((c = fgetc(inputFile)) != EOF && !encoderFlag){
+        if(runningDebug)
+             printf("%d",c);
+        if( 'A' <= c && c <= 'Z'){
+            c = 46; 
+            lettersChaged ++;
+        }
+        if(runningDebug)
+            printf(" %d\n" ,c );
+        strcat(output , c);
+     }
+    if (runningDebug)
+        printf("%d", lettersChaged);
+    printf("%s", output);
 }
