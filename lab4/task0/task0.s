b7da313b9915da4c70f6392b934cee4faeb805f4
spl211 <ishai.picus@gmail.com>
2022-11-18 12:41:52 +0200

    task0 complete

--- lab4/task0/task0.s
+++ lab4/task0/task0.s
@@ -1,6 +1,7 @@
 
         segment .data
-format  db      "%s",0x0a,0
+format1      db      "%s",0x0a,0
+format2      db     "%d",10,0
         segment .text
         global  main            ; let the linker know about main
         extern  printf          ; resolve printf from libc
@@ -8,15 +9,12 @@ format  db      "%s",0x0a,0
 main:
         push    ebp             ; prepare stack frame for main
         mov     ebp, esp
-        sub     esp, 8
-        mov     edi, dword[ebp+8]    ; get argc into edi
-        mov     esi, dword[ebp+12]   ; get first argv string into esi
-        push    dword [esi]     ; must dereference esi; points to argv
-        push    format
+        push    dword [ebp+8]     ; must dereference esi; points to argv
+        push    format2
         call    printf
-        add     esi, 4          ; esi now points to the next argument
+
+        mov     esi, [ebp + 12]          ; esi now points to the next argument
         push    dword [esi]     ; must dereference esi; points to argv
-        push    format
-        call    printf
+        call    puts
         leave
         ret
