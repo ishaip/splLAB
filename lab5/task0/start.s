d5e2f9a0a0d7601f0504911b1fd54e042faac22f
spl211 <ishai.picus@gmail.com>
2022-11-23 19:53:47 +0200

    creating files

---
+++ lab5/task0/start.s
@@ -0,0 +1,20 @@
+
+        segment .data
+format1   db    "%s",0x0a,0
+format2   db    "%d",10,0
+        segment .text
+        global  main            ; let the linker know about main
+        extern  printf          ; resolve printf from libc
+        extern puts
+main:
+        push    ebp             ; prepare stack frame for main
+        mov     ebp, esp
+        push    dword [ebp+8]     ; must dereference esi; points to argc
+        push    format2         ;using the number print
+        call    printf
+
+        mov     esi, [ebp + 12]          ; esi now points to the first argument
+        push    dword [esi]     ; must dereference esi; points to argv
+        call    puts
+        leave
+        ret
