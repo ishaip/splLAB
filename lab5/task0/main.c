f83edaad8125a436336b7f22c52cf7c0742a8c62
spl211 <ishai.picus@gmail.com>
2022-11-24 18:25:47 +0200

    task0 done

--- lab5/task0/main.c
+++ lab5/task0/main.c
@@ -12,7 +12,19 @@ extern int system_call();
 
 int main (int argc , char* argv[], char* envp[])
 {
-  /*Complete the task here*/
-
+  int i;
+  char * str;
+  str = itoa(argc);
+  system_call(SYS_WRITE,STDOUT, str,strlen(str));
+  system_call(SYS_WRITE,STDOUT," arguments \n", 12);
+  for (i = 0 ; i < argc ; i++)
+    {
+      system_call(SYS_WRITE,STDOUT,"argv[", 5);
+	    str = itoa(i);
+      system_call(SYS_WRITE,STDOUT,str,strlen(str));
+      system_call(SYS_WRITE,STDOUT,"] = ",4);
+      system_call(SYS_WRITE,STDOUT,argv[i],strlen(argv[i]));
+      system_call(SYS_WRITE,STDOUT,"\n",1);
+    }
   return 0;
 }
--- lab5/task0/start.s
+++ lab5/task0/start.s
@@ -1,20 +1,47 @@
-
-        segment .data
-format1   db    "%s",0x0a,0
-format2   db    "%d",10,0
-        segment .text
-        global  main            ; let the linker know about main
-        extern  printf          ; resolve printf from libc
-        extern puts
+segment .data    
+str1: db 10
+len: db 0
+segment .text
+WRITE EQU 4
+STDOUT EQU 1
+    global  main            ; let the linker know about main
+    extern strlen
+    extern printf
 main:
         push    ebp             ; prepare stack frame for main
         mov     ebp, esp
-        push    dword [ebp+8]     ; must dereference esi; points to argc
-        push    format2         ;using the number print
-        call    printf
+        mov     edi, dword[ebp+8]    ; get argc into edi       
+        mov     esi, dword[ebp+12]   ; get first argv string into esi
+start_loop:
+        pushad
+        push esi
+        call strlen
+        mov [len], eax
+        add esp, 4
+        popad
 
-        mov     esi, [ebp + 12]          ; esi now points to the first argument
-        push    dword [esi]     ; must dereference esi; points to argv
-        call    puts
+        pushad  
+        mov eax, WRITE		;sys_write
+	mov ebx, STDOUT		;fd of stdout which is unbuffered
+	mov ecx, dword [esi]		;pointer to buffer with data
+	mov edx, dword [len]		;length of data to be written (4 bytes)
+	int 0x80
+        popad
+
+        pushad  
+        mov eax, WRITE		;sys_write
+	mov ebx, STDOUT		;fd of stdout which is unbuffered
+        mov ecx, str1
+        mov edx, 1
+        int 0x80
+        popad			;restore fd in ebx from stack
+
+        mov [len], dword 0
+        add     esi, 4          ; advance to the next pointer in argv
+        dec     edi             ; decrement edi from argc to 0
+        cmp     edi, 0          ; when it hits 0, we're done
+        jnz     start_loop      ; end with NULL pointer
+end_loop:
+        mov eax,0
         leave
         ret
