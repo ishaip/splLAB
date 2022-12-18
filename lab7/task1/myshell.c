2d388e5ecd17e43a6df92d89b2837b93c152aaea
spl211 <ishai.picus@gmail.com>
2022-12-18 16:38:24 +0200

    small toch ups

--- lab7/task1/myshell.c
+++ lab7/task1/myshell.c
@@ -8,22 +8,19 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-int debug =0;
+int debug_mode =0;
 
 void execute(cmdLine *pCmdLine){
-    //execvp replaces the current process with the new process, keeping some of the file handles open.
-    //If we want to launch a new process, you must use fork() and call execvp() in the child process.
     
     // quit command need to execute by parent process
     if (!strcmp("quit",pCmdLine->arguments[0])){
-           fprintf(stdout,"%s\n","Program terminated");
+           fprintf(stdout,"%s\n","quiting");
            freeCmdLines(pCmdLine);
            exit (0);
         }
-    // cd command need to execute by parent process
     if (!strcmp("cd",pCmdLine->arguments[0]) ){
         if(chdir(pCmdLine->arguments[1])==-1){
-           fprintf(stderr,"faild to execute cd command with error number : %d\n",errno);     
+           fprintf(stderr,"cd lacks args or invalid args :\n");     
         }
         freeCmdLines(pCmdLine);
         return;
@@ -31,17 +28,17 @@ void execute(cmdLine *pCmdLine){
     int status;
     int pid = fork();
 
-    if (pid < 0) { // The call to fork failed for some reason. You must take evasive action. A system dependent error message will be waiting in msg.
+    if (pid < 0) { // fork failed
         _exit(1);
     }
     else if (pid == 0 ){ // You are in the child process. You can call exec to start another process. If that fails, you should probably call exit.
-            if (debug){
-                fprintf(stderr, "The command was executed as part of the child process with id :%d\n",pid);
+            if (debug_mode){
+                fprintf(stderr, "child process id :%d\n",pid);
             }
             if (pCmdLine->inputRedirect != NULL){
                 int fd = open(pCmdLine->inputRedirect, O_RDONLY);
                 if(fd == -1 ){
-                    perror("invalid input redirection");
+                    perror("invalid input");
                     _exit(1);
                 }
                 close(0);
@@ -51,7 +48,7 @@ void execute(cmdLine *pCmdLine){
             if (pCmdLine->outputRedirect != NULL){
                 int fd = open(pCmdLine->outputRedirect, O_WRONLY | O_CREAT, 0644);
                 if(fd == -1 ){
-                    perror("invalid output redirection");
+                    perror("invalid output");
                     _exit(1);
                 }
                 close(1);
@@ -59,13 +56,13 @@ void execute(cmdLine *pCmdLine){
                 close(fd);	// the old fb is still open
             }
             if (execvp(pCmdLine->arguments[0],pCmdLine->arguments) == -1) {
-                    perror("An error has occured");
+                    perror("execvp error");
                     _exit(1); // need to exit cause we dont want the process to continue (end with process of the command)
             }
         }
     else if (pid > 0) { // You are in the parent process. The value returned from fork is the process id of the child process. You should probably arrange to wait for any child processes to exit.
-            if (debug){
-             fprintf(stderr, "the parnet process with id: %d is waiting for child process\n",pid);
+            if (debug_mode){
+             fprintf(stderr, "parnet process id %dis wating\n",pid);
             }
             if (pCmdLine->blocking){ // when command finished with & (blocking set to 0) the process isnt blocking the shell process
             waitpid(pid,&status,0); // waitpid() functions shall obtain status information of child when available , return pid of the child and stored the status at the adress given
@@ -82,7 +79,7 @@ int main(int argc, char** argv) {
     cmdLine* cmdLine;
     for (i = 0; i<argc;i++){
         if (!strncmp("-d",argv[i],2)){
-            debug=1;
+            debug_mode=1;
         }
     }
         
@@ -94,7 +91,7 @@ int main(int argc, char** argv) {
         char* ret_val = fgets(command,2048,stdin);
         // NULL on error or when end of file occurs while no characters have been read.
         
-        if (debug){
+        if (debug_mode){
             fprintf(stderr,"The command is: %s\n",command);
         }
         
@@ -102,7 +99,7 @@ int main(int argc, char** argv) {
         execute(cmdLine);
         }
         else{
-                fprintf(stdout,"%s\n","Illegal command");
+                fprintf(stdout,"%s\n","cannot parse command");
         }
     }
     return 0;
--- lab7/task2/mypipe.c
+++ lab7/task2/mypipe.c
@@ -13,50 +13,49 @@
 
 int main(int argc, char **argv){
 	int pipefd[2];
-	pid_t cpid1,cpid2;
+	pid_t child_pid1,child_pid2;
 	int ret_val;
 
 	if (pipe(pipefd) == -1) {
-		perror("pipe");
+		perror("pipe error");
 		exit(EXIT_FAILURE);
 	}
 
-	cpid1 = fork();
-	if (cpid1 == -1) {
-		perror("fork");
+	child_pid1 = fork();
+	if (child_pid1 == -1) {
+		perror("fork error");
 		exit(EXIT_FAILURE);
 	}
 
-	if (cpid1 == 0) {    /* Child reads from pipe */
+	if (child_pid1 == 0) {    /* Child reads from pipe */
 		close(stdout);
-		dup(pipefd[1]);  /* dup copy fd to lowest fd avaliable and its stdout since it has been closed */
-		/* now stdout is the write end of the pipe */
+		dup(pipefd[1]);  
 		close(pipefd[1]);
 		char* my_argv[3] = {"ls", "-l", 0};
 		execvp(my_argv[0], my_argv);
         _exit(0);
 	} 
-	if(cpid1>0) {            /* Parent writes argv[1] to pipe */
+	if(child_pid1>0) {            /* Parent writes argv[1] to pipe */
 		
-		cpid2 = fork();
-		if (cpid2 == -1) {
-			perror("fork");
+		child_pid2 = fork();
+		if (child_pid2 == -1) {
+			perror("fork error");
 			exit(EXIT_FAILURE);
 		}
 
-		if (cpid2 == 0) {    /* Child reads from pipe */
+		if (child_pid2 == 0) {    /* Child reads from pipe */
 			close(stdin);
 			dup(pipefd[0]);
 			close(pipefd[0]);
-			char* my_argv[5] = {"tail", "-n","2","in.txt" , 0};
+			char* my_argv[5] = {"tail", "-n","2","in.txt" , 0};		//do we need this to be an adress?
 			execvp(my_argv[0], my_argv);
         	_exit(0);
 		} 
 		
-		if(cpid2>0) { 
+		if(child_pid2>0) { 
 			close(pipefd[0]);
-			waitpid(cpid1,&ret_val,0);		/* Wait for child */
-			waitpid(cpid2,&ret_val,0);		/* Wait for child */
+			waitpid(child_pid1,&ret_val,0);	
+			waitpid(child_pid2,&ret_val,0);	/* ret val isn't relevent we just want both childern to finish */
 			exit(0);
 		}
 	}
