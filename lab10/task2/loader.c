860bb2776c2356882e3ad294aff51badac7b749f
spl211 <ishai.picus@gmail.com>
2023-01-09 16:33:54 +0200

    lab10 done small fixes remain

--- lab10/task2/loader.c
+++ lab10/task2/loader.c
@@ -20,9 +20,9 @@ Elf32_Ehdr *header; /* this will point to the header structure */
 int LoadFile(char*);
 void examineFile(char*);
 void printProgramHeaders();
-char* convertType(int);
-char* flagToReadWrite(int);
-int flagToPermissions(int flg);
+char* mapping_type_to_string(int);
+char* mapping_to_writeRead(int);
+int mapping_to_protection(int flg);
 
 
 
@@ -59,13 +59,13 @@ void load_phdr(Elf32_Phdr *phdr, int fd)
 {
 
     printf("%s\t\t%#08x\t%#08x\t%#10.08x\t%#07x\t%#07x\t%s\t%#-6.01x\n",
-        convertType(phdr->p_type),phdr->p_offset,phdr->p_vaddr,phdr->p_paddr,phdr->p_filesz,phdr->p_memsz,flagToReadWrite(phdr->p_flags),phdr->p_align);
+        mapping_type_to_string(phdr->p_type),phdr->p_offset,phdr->p_vaddr,phdr->p_paddr,phdr->p_filesz,phdr->p_memsz,mapping_to_writeRead(phdr->p_flags),phdr->p_align);
     if(phdr->p_type == PT_LOAD)
     {  
         void *vadd = (void *)(phdr -> p_vaddr & 0xfffff000);
         int offset = phdr -> p_offset & 0xfffff000;
         int padding = phdr -> p_vaddr & 0xfff;
-        int convertedFlag = flagToPermissions(phdr -> p_flags);
+        int convertedFlag = mapping_to_protection(phdr -> p_flags);
         void* temp;
         if ((temp = mmap(vadd, phdr -> p_memsz+padding, convertedFlag, MAP_FIXED | MAP_PRIVATE, fd, offset)) == MAP_FAILED ) {
             perror("mmap failed1");
@@ -74,55 +74,52 @@ void load_phdr(Elf32_Phdr *phdr, int fd)
     }
 }
 
-char* convertType(int type){
-    switch (type){
-        case PT_NULL: return "NULL";
-        case PT_LOAD: return "LOAD";
-        case PT_DYNAMIC: return "DYNAMIC";
-        case PT_INTERP: return "INTERP";
-        case PT_NOTE: return "NOTE";
-        case PT_SHLIB: return "SHLIB";
-        case PT_PHDR: return "PHDR";
-        case PT_TLS: return "TLS";
-        case PT_NUM: return "NUM";
-        case PT_GNU_EH_FRAME: return "GNU_EH_FRAME";
-        case PT_GNU_STACK: return "GNU_STACK";
-        case PT_GNU_RELRO: return "GNU_RELRO";
-        case PT_SUNWBSS: return "SUNWBSS";
-        case PT_SUNWSTACK: return "SUNWSTACK";
-        case PT_HIOS: return "HIOS";
-        case PT_LOPROC: return "LOPROC";
-        case PT_HIPROC: return "HIPROC"; 
-        default:return "Unknown";
-    }
+
+char* mapping_type_to_string(int flag){
+        if (flag == PT_NULL) return "NULL";
+        if (flag == PT_LOAD) return "LOAD";
+        if (flag == PT_DYNAMIC) return "DYNAMIC";
+        if (flag == PT_INTERP) return "INTERP";
+        if (flag == PT_NOTE) return "NOTE";
+        if (flag == PT_SHLIB) return "SHLIB";
+        if (flag == PT_PHDR) return "PHDR";
+        if (flag == PT_TLS) return "TLS";
+        if (flag == PT_NUM) return "NUM";
+        if (flag == PT_GNU_EH_FRAME) return "GNU_EH_FRAME";
+        if (flag == PT_GNU_STACK) return "GNU_STACK";
+        if (flag == PT_GNU_RELRO) return "GNU_RELRO";
+        if (flag == PT_SUNWBSS) return "SUNWBSS";
+        if (flag == PT_SUNWSTACK) return "SUNWSTACK";
+        if (flag == PT_HIOS) return "HIOS";
+        if (flag == PT_LOPROC) return "LOPROC";
+        if (flag == PT_HIPROC) return "HIPROC"; 
+    return "ERROR";
+
 }
 
-char* flagToReadWrite(int flg){
-    switch (flg){
-        case 0x000: return "";
-        case 0x001: return "E";
-        case 0x002: return "W";
-        case 0x003: return "WE";
-        case 0x004: return "R";
-        case 0x005: return "RE";
-        case 0x006: return "RW";
-        case 0x007: return "RWE";
-        default:return "Unknown";
-    }
+char* mapping_to_writeRead(int flag){
+    if(flag == 0) return "";
+    if(flag == 1) return "E";
+    if(flag == 2) return "W";
+    if(flag == 3) return "WE";
+    if(flag == 4) return "R";
+    if(flag == 5) return "RE";
+    if(flag == 6) return "RW";
+    if(flag == 7) return "RWE";
+    return "ERROR";
 }
 
-int flagToPermissions(int flg){
-    switch (flg){
-        case 0x000: return 0;
-        case 0x001: return PROT_EXEC;
-        case 0x002: return PROT_WRITE;
-        case 0x003: return PROT_EXEC | PROT_EXEC;
-        case 0x004: return PROT_READ;
-        case 0x005: return PROT_READ | PROT_EXEC;
-        case 0x006: return PROT_READ | PROT_WRITE;
-        case 0x007: return PROT_READ | PROT_WRITE | PROT_EXEC;
-        default:return -1;
-    }
+//maping the number of flag to the correct permession
+int mapping_to_protection(int flag){
+    if(flag == 0) return 0;
+    if(flag == 1) return PROT_EXEC;
+    if(flag == 2) return PROT_WRITE;
+    if(flag == 3) return PROT_EXEC | PROT_EXEC;
+    if(flag == 4) return PROT_READ;
+    if(flag == 5) return PROT_READ | PROT_EXEC;
+    if(flag == 6) return PROT_READ | PROT_WRITE;
+    if(flag == 7) return PROT_READ | PROT_WRITE | PROT_EXEC;
+    return -1;
 }
 
 int main(int argc, char **argv){
@@ -139,6 +136,7 @@ int main(int argc, char **argv){
      else
      {
         printf("This is not ELF file\n");
+        //clear the map function allocated memory "un-maping"
         munmap(map_start, fd_stat.st_size); 
         close(currFD); 
         currFD=-1;
